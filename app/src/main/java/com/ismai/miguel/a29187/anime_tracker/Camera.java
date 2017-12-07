3c60fffd89ca2886f1896325c1195e911e4cd09c
Miguel Silva <miguel.lvl14@gmail.com>
2017-12-07 12:10:48 +0000

    Added Navigation with Tabs(Placeholder)

---
+++ app/src/main/java/com/ismai/miguel/a29187/anime_tracker/Camera.java
@@ -0,0 +1,17 @@
+package com.ismai.miguel.a29187.anime_tracker;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Camera extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.camera, container, false);
+
+        return rootView;
+    }
+}
